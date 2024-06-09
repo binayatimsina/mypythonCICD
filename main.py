59a10982b19f45c78fef3115de288a827a6b5130
Binaya Timsina <btimsina3@gatech.edu>
2024-06-09 18:56:43 -0400

    Create Binaya Route

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ application = Flask(__name__)
 def hello():
     return 'Hello, This is Binaya Timsina and this is his website!'
 
+@application.route('/binaya')
+def binaya():
+    return 'Binaya Timsina'
+
 if __name__ == '__main__':
     application.run(debug=True)
